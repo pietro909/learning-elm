0ceeee5e957ec1584bd48ae671eda365abbc1a63
Pietro Grandi <pietro.grandi@betsson.com>
2016-06-09 15:24:32 +0200

    get real time and add minutes

--- myClock.elm
+++ myClock.elm
@@ -3,6 +3,8 @@ import Html.App as App
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
+import Date exposing (..)
+import Debug
 
 
 main =
@@ -16,11 +18,11 @@ main =
 
 -- MODEL
 
-type alias Model = Time
+type alias Model = Date
 
 init : (Model, Cmd Msg)
 init =
-    (0, Cmd.none)
+    (Date.fromTime 1.0, Cmd.none)
 
 
 -- UPDATE
@@ -32,14 +34,17 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update action model =
     case action of
         Tick newTime ->
-            (newTime, Cmd.none)
+            let 
+                date = Date.fromTime newTime
+            in
+                (date, Cmd.none)
 
 
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Time.every second Tick
+    Time.every Time.second Tick
 
 
 -- VIEW
@@ -47,9 +52,9 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     let
-        angle =
-            turns (Time.inMinutes model)
-
+        seconds = Date.second model
+        turn = (toFloat(seconds) / 60) - 0.25
+        angle = turns turn
         handX =
             toString (50 + 40 * cos angle)
 
@@ -60,4 +65,16 @@ view model =
         svg [ viewBox "0 0 100 100", width "300px" ]
             [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
             , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+            , lineMinutes model
             ]
+
+lineMinutes : Date -> Html Msg
+lineMinutes model =
+    let
+        seconds = Date.minute model
+        turn = (toFloat(seconds) / 60) - 0.25
+        angle = turns turn
+        handX = toString (50 + 35 * cos angle)
+        handY = toString (50 + 35 * sin angle)
+    in
+       line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#663963" ] []
