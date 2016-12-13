5d5226eb2cfe31ea4a2986ebbd1afe39278e0299
pietro909 <pietro.grandi.3d@gmail.com>
2016-12-13 19:20:32 +0100

    Add type annotations and refactor

--- elm-in-action/PhotoGroove.elm
+++ elm-in-action/PhotoGroove.elm
@@ -3,9 +3,29 @@ module PhotoGroove exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Array exposing (Array)
 
+type alias Photo =
+  { url : String }
+
+type alias Model =
+  { photos : List Photo
+  , selectedUrl : String
+  }
+
+type alias Msg =
+  { operation : String
+  , data : String
+  }
+
+urlPrefix : String
 urlPrefix = "http://elm-in-action.com/"
 
+photoArray : Array Photo
+photoArray =
+  Array.fromList initialModel.photos
+
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumbnail =
   img
     [ src (urlPrefix ++ thumbnail.url)
@@ -14,6 +34,7 @@ viewThumbnail selectedUrl thumbnail =
     ]
     []
 
+view : Model -> Html Msg
 view model =
   div [ class "content" ]
     [ h1 [] [ text "Photo Groove" ]
@@ -26,6 +47,7 @@ view model =
       []
     ]
 
+initialModel : Model
 initialModel =
   { photos = 
     [ { url = "1.jpeg" }
@@ -35,6 +57,7 @@ initialModel =
   , selectedUrl = "1.jpeg"
   }
 
+update : Msg -> Model -> Model
 update msg model =
   if msg.operation == "SELECT_PHOTO" then
     { model | selectedUrl = msg.data }
