ad8fe289467e00376cc497160c890961bc54eb3d
pietro909 <pietro.grandi.3d@gmail.com>
2016-12-13 11:03:10 +0100

    Add user interaction, end of Chapter 2.

--- elm-in-action/PhotoGroove.elm
+++ elm-in-action/PhotoGroove.elm
@@ -2,17 +2,48 @@ module PhotoGroove exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+urlPrefix = "http://elm-in-action.com/"
+
+viewThumbnail selectedUrl thumbnail =
+  img
+    [ src (urlPrefix ++ thumbnail.url)
+    , classList [ ( "selected", selectedUrl == thumbnail.url) ]
+    , onClick { operation = "SELECT_PHOTO", data = thumbnail.url }
+    ]
+    []
 
 view model =
   div [ class "content" ]
     [ h1 [] [ text "Photo Groove" ]
     , div [ id "thumbnails" ]
-      [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-      , img [ src "http://elm-in-action.com/2.jpeg" ] []
-      , img [ src "http://elm-in-action.com/3.jpeg" ] []
+      (List.map (viewThumbnail model.selectedUrl) model.photos)
+    , img
+      [ class "large"
+      , src (urlPrefix ++ "large/" ++ model.selectedUrl)
       ]
+      []
     ]
 
-main =
-  view "no model yet"
+initialModel =
+  { photos = 
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
+  , selectedUrl = "1.jpeg"
+  }
 
+update msg model =
+  if msg.operation == "SELECT_PHOTO" then
+    { model | selectedUrl = msg.data }
+  else
+    model
+
+main =
+  Html.beginnerProgram
+    { model = initialModel
+    , view = view
+    , update = update
+    }
